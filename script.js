122e9d7f7f586ad54d200fdc37f2fb929405ff26
CoderGirl <angi.adema@gmail.com>
2023-11-14 14:51:34 -0700

    added notes and made the code drier in script.js

--- script.js
+++ script.js
@@ -18,16 +18,21 @@ openModal.addEventListener("click", () => {
   overlay.classList.add("open")
 })
 
-// TODO: 4. Create a click event listener for the close-modal-btn that removes the class "open" from the modal
-closeModal.addEventListener("click", () => {
+// To remove duplicate code in the code blocks below, we add the function and just call this function in both code blocks.
+const closeTheModal = () => {
   modal.classList.remove("open")
-
-  // BONUS: Also remove the class "open" from the overlay
   overlay.classList.remove("open")
-})
+}
+
+// TODO: 4. Create a click event listener for the close-modal-btn that removes the class "open" from the modal
+closeModal.addEventListener("click", closeTheModal)
+//   modal.classList.remove("open")
+// BONUS: Also remove the class "open" from the overlay
+//   overlay.classList.remove("open")
+//   closeTheModal() // Get rid of the annonamous function in the event listener and just pass in the closeTheModal function to be called since it is the last thing.
 
 // BONUS: Add a click event listener to the overlay that removes the class "open" from the modal and the overlay
-overlay.addEventListener("click", () => {
-  modal.classList.remove("open")
-  overlay.classList.remove("open")
-})
+overlay.addEventListener("click", closeTheModal)
+//   modal.classList.remove("open")
+//   overlay.classList.remove("open")
+//   closeTheModal()  // Get rid of the annonamous function in the event listener and just pass in the closeTheModal function to be called since it is the last thing.
